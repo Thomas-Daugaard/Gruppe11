ca5d28707b06fcc9cb8082da45012d7f978c5d59
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-03-23 09:19:31 +0100

    Added new library project, console app and test project

---
+++ SomeConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
