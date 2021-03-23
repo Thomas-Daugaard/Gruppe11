3b8bef20a089d5a5ae47f1346ce30d36d0ebc743
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-03-23 10:59:50 +0100

    Changed method names

--- Bilka2.0/IProductComponenet.cs
+++ Bilka2.0/IProductComponenet.cs
@@ -4,12 +4,12 @@ namespace Bilka
 {
     public interface IProductComponent
     {
-        public void AddProduct(Product);
-        public void RemoveProduct(Product);
-        public Product GetProduct();
+        public void AddComponent(IProductComponent product);
+        public void RemoveComponent(IProductComponent product);
+        public IProductComponent GetComponent();
         public void PrintProduct();
         public double GetTotalValue();
-        public int NumberOfProducts { get; set; }
+        public int NumberOfComponents { get; set; }
         public string Description { get; set; }
     }
 }
