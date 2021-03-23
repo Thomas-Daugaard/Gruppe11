b3f64b00eefe5d0f58c3ba08a5544e9a39872265
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-23 11:10:29 +0100

    dd

---
+++ Bilka2.0/ProductGroup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bilka;
+
+namespace Bilka2._0
+{
+    public class ProductGroup: IProductComponent
+    {
+        public void AddProduct(IProductComponent p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProduct(IProductComponent p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IProductComponent GetProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTotalValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumberOfProducts { get; set; }
+        public string Description { get; set; }
+    }
+}
