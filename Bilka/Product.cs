61aa8c6e7ed3261d43d7b3e4f2b9c91a95b23284
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-25 09:12:37 +0100

    added subcategories to product class

--- Bilka/Product.cs
+++ Bilka/Product.cs
@@ -5,12 +5,18 @@ namespace Bilka
 {
     public class Product : IProductComponent
     {
+        public Product()
+        {
+            _subproductCategories = new List<IProductComponent>();
+        }
         public void Print()
         {
             Console.WriteLine(Name + "with description: " + Description);
         }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        private List<IProductComponent> _subproductCategories;
 
 
     }
